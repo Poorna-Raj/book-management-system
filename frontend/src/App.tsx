17ba6e90d085b66b8b6d8f75a32045f0bab5c327
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-01-30 13:57:05 +0530

    define the test page for testing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import Test from "./pages/Test";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/about" element={<Dashboard />} />
+      <Route path="/test" element={<Test/>}/>
     </Routes>
   );
 }
