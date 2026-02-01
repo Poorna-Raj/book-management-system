7b38cf0e88d87b5369eab275a3dfda8d6ae0a913
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-02-01 15:51:35 +0530

    define the dto class for the customer create request

---
+++ backend/src/main/java/com/abbys/bms/dto/CustomerCreateRequest.java
@@ -0,0 +1,76 @@
+package com.abbys.bms.dto;
+
+public class CustomerCreateRequest {
+    private String customerName;
+    private int age;
+    private String address;
+    private String nic;
+    private String contactNumber;
+    private String email;
+    private String gender;
+    private int managerId;
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(int managerId) {
+        this.managerId = managerId;
+    }
+}
