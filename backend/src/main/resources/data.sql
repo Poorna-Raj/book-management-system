a2e6c27f2f7fa4e5c9e52cf154aecbd517482e0b
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-02-01 20:17:42 +0530

    add supplier manager

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,4 +1,9 @@
 INSERT INTO tbl_users (email, password, name, contact_number, bank_number, bank_name, role)
-VALUES ('manager@example.com', 'SecurePassword123', 'John Doe', '0771234567', '1234567890', 'ABC Bank', 'CUSTOMER_MANAGER');
+VALUES ('customer@example.com', 'customer123', 'Steve Jobs', '0771234567', '1234567890', 'ABC Bank', 'CUSTOMER_MANAGER');
 INSERT INTO tbl_customer_managers (user_id, customer_manager_id, working_hours, shift, on_duty)
 VALUES (1, 'CM001', '9AM-5PM', 'FULL', TRUE);
+
+INSERT INTO tbl_users (email, password, name, contact_number, bank_number, bank_name, role)
+VALUES ('supplier@example.com', 'supplier123', 'Tom Brady', '0775734587', '93748737890', 'BAC Bank', 'SUPPLIER_MANAGER');
+INSERT INTO tbl_supplier_manager (user_id, supplier_manager_id, working_hours, shift, on_duty)
+VALUES (2, 'SM001', '9AM-5PM', 'FULL', TRUE);
