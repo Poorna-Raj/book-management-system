f12596331101904a8f239294844dc4f78a3df4f2
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-02-01 15:24:12 +0530

    define the find by email method for user repo

--- backend/src/main/java/com/abbys/bms/reposiotory/UserRepo.java
+++ backend/src/main/java/com/abbys/bms/reposiotory/UserRepo.java
@@ -3,5 +3,8 @@ package com.abbys.bms.reposiotory;
 import com.abbys.bms.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepo extends JpaRepository<User,Integer> {
+    Optional<User> findByEmail(String email);
 }
