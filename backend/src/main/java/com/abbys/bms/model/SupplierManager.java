602313decd3fd019e05358d3af5bf21702fb59a9
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-02-01 16:27:45 +0530

    define the model class for supplier manager

---
+++ backend/src/main/java/com/abbys/bms/model/SupplierManager.java
@@ -0,0 +1,74 @@
+package com.abbys.bms.model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tbl_supplier_manager")
+public class SupplierManager extends User{
+    @Column(unique = true)
+    private String supplierManagerId;
+
+    private String workingHours;
+
+    @Enumerated(EnumType.STRING)
+    private Shift shift;
+
+    private boolean onDuty;
+
+    @OneToMany(mappedBy = "manager", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Supplier> suppliers = new ArrayList<>();
+
+    public List<Supplier> getSuppliers() {
+        return suppliers;
+    }
+
+    public void setSuppliers(List<Supplier> suppliers) {
+        this.suppliers = suppliers;
+    }
+
+    public void addCustomer(Supplier supplier) {
+        suppliers.add(supplier);
+        supplier.setManager(this); // maintain both sides
+    }
+
+    public void removeCustomer(Supplier supplier) {
+        suppliers.remove(supplier);
+        supplier.setManager(null);
+    }
+
+
+    public String getSupplierManagerId() {
+        return supplierManagerId;
+    }
+
+    public void setSupplierManagerId(String supplierManagerId) {
+        this.supplierManagerId = supplierManagerId;
+    }
+
+    public String getWorkingHours() {
+        return workingHours;
+    }
+
+    public void setWorkingHours(String workingHours) {
+        this.workingHours = workingHours;
+    }
+
+    public Shift getShift() {
+        return shift;
+    }
+
+    public void setShift(Shift shift) {
+        this.shift = shift;
+    }
+
+    public boolean isOnDuty() {
+        return onDuty;
+    }
+
+    public void setOnDuty(boolean onDuty) {
+        this.onDuty = onDuty;
+    }
+}
