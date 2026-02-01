825bbdadadaf051a273f355ef6419562f745899a
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-02-02 00:08:15 +0530

    define the customer order model class

---
+++ backend/src/main/java/com/abbys/bms/model/CustomerOrder.java
@@ -0,0 +1,66 @@
+package com.abbys.bms.model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tbl_customer_orders")
+public class CustomerOrder extends Order{
+    private String importantNote;
+
+    private String deadline;
+
+    @OneToMany
+    @JoinTable(
+            name = "tbl_customer_order_books",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "book_id")
+    )
+    private List<Book> books = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void removeBook(Book book) {
+        books.remove(book);
+    }
+
+    public String getImportantNote() {
+        return importantNote;
+    }
+
+    public void setImportantNote(String importantNote) {
+        this.importantNote = importantNote;
+    }
+
+    public String getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(String deadline) {
+        this.deadline = deadline;
+    }
+}
