e2d7438beed96cd52cf340c2ff5fb36f66850b6a
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-02-01 16:29:23 +0530

    define jpa repository for the supplier manager model

---
+++ backend/src/main/java/com/abbys/bms/reposiotory/SupplierManagerRepo.java
@@ -0,0 +1,7 @@
+package com.abbys.bms.reposiotory;
+
+import com.abbys.bms.model.SupplierManager;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupplierManagerRepo extends JpaRepository<SupplierManager,Integer> {
+}
