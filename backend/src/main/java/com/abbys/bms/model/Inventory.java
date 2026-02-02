8bf46bff9f8824bf7e77a982a8dd2fca75d4a614
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-02-02 20:33:20 +0530

    define the model class of the inventory

---
+++ backend/src/main/java/com/abbys/bms/model/Inventory.java
@@ -0,0 +1,74 @@
+package com.abbys.bms.model;
+
+import com.abbys.bms.model.enums.InventoryStatus;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tbl_inventory")
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long inventoryId;
+
+    @Column(nullable = false)
+    private String inventoryTitle;
+
+    @Column(nullable = false)
+    private int totalBooks;
+
+    @Enumerated(EnumType.STRING)
+    private InventoryStatus status;
+
+    @Column(nullable = true)
+    private String note;
+
+    @OneToMany(
+            mappedBy = "inventory",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<Book> books = new ArrayList<>();
+
+    public Long getInventoryId() {
+        return inventoryId;
+    }
+
+    public void setInventoryId(Long inventoryId) {
+        this.inventoryId = inventoryId;
+    }
+
+    public String getInventoryTitle() {
+        return inventoryTitle;
+    }
+
+    public void setInventoryTitle(String inventoryTitle) {
+        this.inventoryTitle = inventoryTitle;
+    }
+
+    public int getTotalBooks() {
+        return totalBooks;
+    }
+
+    public void setTotalBooks(int totalBooks) {
+        this.totalBooks = totalBooks;
+    }
+
+    public InventoryStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(InventoryStatus status) {
+        this.status = status;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
