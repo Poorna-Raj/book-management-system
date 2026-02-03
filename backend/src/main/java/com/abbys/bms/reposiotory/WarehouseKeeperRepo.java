455d1a88757bcc1392c6d01fd3b76c0266bfe273
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-02-03 21:05:28 +0530

    define the repository class for wearhouse keeper

---
+++ backend/src/main/java/com/abbys/bms/reposiotory/WarehouseKeeperRepo.java
@@ -0,0 +1,7 @@
+package com.abbys.bms.reposiotory;
+
+import com.abbys.bms.model.WarehouseKeeper;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WarehouseKeeperRepo extends JpaRepository<WarehouseKeeper,Integer> {
+}
