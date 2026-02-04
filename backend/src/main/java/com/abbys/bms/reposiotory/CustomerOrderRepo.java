3a4ca53b65cd063b4eccd7df4d63fd3612c65443
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-02-04 22:23:10 +0530

    define the repository class for customer order repo

--- backend/src/main/java/com/abbys/bms/reposiotory/CustomerOrderRepo.java
+++ backend/src/main/java/com/abbys/bms/reposiotory/CustomerOrderRepo.java
@@ -1,7 +1,27 @@
 package com.abbys.bms.reposiotory;
 
+import com.abbys.bms.dto.report.CustomerPurchaseReport;
 import com.abbys.bms.model.CustomerOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CustomerOrderRepo extends JpaRepository<CustomerOrder,Long> {
+    @Query("""
+        SELECT new com.abbys.bms.dto.report.CustomerPurchaseReport(
+            c.customerName,
+            c.email,
+            o.orderId,
+            o.totalPrice,
+            o.orderStatus,
+            b.bookName,
+            ca.name
+        )
+        FROM CustomerOrder o
+        JOIN o.customer c
+        JOIN o.cashier ca
+        JOIN o.books b
+    """)
+    List<CustomerPurchaseReport> getCustomerPurchaseReport();
 }
