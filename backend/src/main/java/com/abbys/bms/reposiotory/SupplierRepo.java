f7e1254256d2eddb727b8d3dfa035a5e8db6a815
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-02-04 22:23:46 +0530

    define the repository class for supplier

--- backend/src/main/java/com/abbys/bms/reposiotory/SupplierRepo.java
+++ backend/src/main/java/com/abbys/bms/reposiotory/SupplierRepo.java
@@ -1,7 +1,25 @@
 package com.abbys.bms.reposiotory;
 
+import com.abbys.bms.dto.report.SupplierPerformanceReport;
 import com.abbys.bms.model.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SupplierRepo extends JpaRepository<Supplier,Long> {
+    @Query("""
+        SELECT new com.abbys.bms.dto.report.SupplierPerformanceReport(
+            s.companyName,
+            sm.name,
+            COUNT(b),
+            SUM(b.stock),
+            AVG(b.price)
+        )
+        FROM Supplier s
+        JOIN s.books b
+        LEFT JOIN s.manager sm
+        GROUP BY s.companyName, sm.name
+    """)
+    List<SupplierPerformanceReport> getSupplierPerformanceReport();
 }
