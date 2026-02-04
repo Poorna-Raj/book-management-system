794e4f7235e63ec988ed4323fd3e18fe3b3e9fcf
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-02-04 16:29:49 +0530

    define the jql method for the inventory report

--- backend/src/main/java/com/abbys/bms/reposiotory/InventoryRepo.java
+++ backend/src/main/java/com/abbys/bms/reposiotory/InventoryRepo.java
@@ -1,7 +1,27 @@
 package com.abbys.bms.reposiotory;
 
+import com.abbys.bms.dto.report.InventoryStockReport;
 import com.abbys.bms.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface InventoryRepo extends JpaRepository<Inventory,Long> {
+    @Query("""
+        SELECT new com.abbys.bms.dto.report.InventoryStockReport(
+            i.inventoryTitle,
+            b.bookName,
+            b.isbn,
+            b.stock,
+            b.price,
+            s.companyName,
+            wk.name
+        )
+        FROM Inventory i
+        JOIN i.books b
+        LEFT JOIN b.supplier s
+        LEFT JOIN b.warehouseKeeper wk
+    """)
+    List<InventoryStockReport> getInventoryStockReport();
 }
