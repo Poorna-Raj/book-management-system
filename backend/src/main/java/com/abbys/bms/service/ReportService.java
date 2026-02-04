d45f82e0c3b1da91bc0e095d5b3f88d5550c9aa1
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-02-04 22:24:11 +0530

    define the service class for report

--- backend/src/main/java/com/abbys/bms/service/ReportService.java
+++ backend/src/main/java/com/abbys/bms/service/ReportService.java
@@ -1,7 +1,11 @@
 package com.abbys.bms.service;
 
+import com.abbys.bms.dto.report.CustomerPurchaseReport;
 import com.abbys.bms.dto.report.InventoryStockReport;
+import com.abbys.bms.dto.report.SupplierPerformanceReport;
+import com.abbys.bms.reposiotory.CustomerOrderRepo;
 import com.abbys.bms.reposiotory.InventoryRepo;
+import com.abbys.bms.reposiotory.SupplierRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +15,16 @@ import java.util.List;
 public class ReportService {
     @Autowired
     private InventoryRepo _inventory;
+    @Autowired
+    private CustomerOrderRepo _order;
+    @Autowired
+    private SupplierRepo _supplier;
 
     public List<InventoryStockReport> getInventoryStockReport(){
         return _inventory.getInventoryStockReport();
     }
+
+    public List<CustomerPurchaseReport> getCustomerReport() { return _order.getCustomerPurchaseReport(); }
+
+    public List<SupplierPerformanceReport> getSupplierPerformanceReport() { return _supplier.getSupplierPerformanceReport(); }
 }
